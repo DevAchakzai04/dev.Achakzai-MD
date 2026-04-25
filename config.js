cce819f03a4d0a928a2e17794b61867adf8452e8
DevAchakzai04 <dev.achakzai04@outlook.com>
2026-04-25 10:11:24 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+import chalk from "chalk";
+
+global.owner = ["923308363433", "923308363433"];
+
+global.sessionName = "Sessions/Owner";
+global.version = "1.0";
+global.pairing_code = true;
+global.number_bot = "";
+global.prefa = ".";
+global.msgglobal =
+  "[Achakzai 04] Welcome to the advance WhatsApp bot!";
+globalThis.dev = "☄️ Deve.Achakzai04 - Achakzai 04 Developer";
+
+global.api = {
+  url: "https://nex-magical.vercel.app",
+  url2: "https://api.vreden.my.id",
+  url3: "https://api-faa.my.id",
+};
+
+global.bot = {
+  name: "Achakzai 04",
+  api: "https://nexevo.onrender.com",
+  web: "https://nexevo.onrender.com",
+  photo: "https://files.catbox.moe/9jbw55.jpg",
+  group: "https://chat.whatsapp.com/LGhlAx9Z6aBEvkhNoUgH30?mode=gi_t",
+      channel: "https://whatsapp.com/channel/0029VbCf0uq9xVJbJhdYjC27",
+  newsletter: "120363407904372384",
+  github: "https://github.com",
+};
+
+global.mods = ["923308363433", "923308363433"];
+
+global.mess = {
+  socket: "🛡 This command can only be executed by the Socket.",
+  admin: "🛡 This command can only be executed by Group Admins.",
+  botAdmin: "🛡 This command can only be executed if the Socket is a Group Admin.",
+};
